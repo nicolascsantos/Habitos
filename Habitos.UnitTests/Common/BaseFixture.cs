f34d226018414b9981eca38b8b0074592ee7ff68
Nicolas de Carvalho Santos <nicolas.carvalho@hotmail.com.br>
2025-07-11 14:28:32 -0300

    adding basefixture

---
+++ Habitos.UnitTests/Common/BaseFixture.cs
@@ -0,0 +1,14 @@
+﻿using Bogus;
+
+namespace Habitos.UnitTests.Common
+{
+    public class BaseFixture
+    {
+        public BaseFixture()
+        {
+            Faker = new Faker("pt-BR");
+        }
+
+        public Faker Faker { get; set; }
+    }
+}
